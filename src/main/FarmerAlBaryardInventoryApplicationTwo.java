6c06611a66a474a0638cc6a4eb889dc3f1c027be
perjohnnym <juanjmore33@gmail.com>
2023-12-27 19:05:45 -0600

    changes

--- src/main/FarmerAlBaryardInventoryApplicationTwo.java
+++ src/main/FarmerAlBaryardInventoryApplicationTwo.java
@@ -3,6 +3,8 @@ package main;
 import main.animals.Pig;
 import main.service.FileService;
 
+import java.util.Arrays;
+
 public class FarmerAlBaryardInventoryApplicationTwo {
 
 	public static void main(String[] args) {
@@ -10,15 +12,36 @@ public class FarmerAlBaryardInventoryApplicationTwo {
 		FileService fileService = new FileService();
 		Pig[] pigs = fileService.getPigsFromFile();
 		String[] pigNames = new String[4];
-  		
+
+		pigNames[0]="Corkey";
+		pigNames[1]="Penny";
+		pigNames[2]="Bartholomew";
+		pigNames[3]="Rocky";
+
+
+		System.out.println("------This is PRE SORT --------");
+		System.out.println("Insert the code here that will write/display the recs as is:");
+
+
+		System.out.println("------This is POST SORT --ASC Order------");
+
+		Arrays.sort(pigNames);
+		for(String piggy : pigNames){
+			System.out.println(getPiggy(piggy));
+		}
+
 		// SORT the pigs by name...
 		
 		// 1. run a for loop on the pig array
 		// 2. In the for loop create an array called pig names, get the names of the pigs using
 		// the getName getter from the Pig class, save it into the pigNames string array
-		// 3. Sort the pig names
+		// 3. Sort the pig names in Ascending order
 		// 4. Print the sorted names to the console
 
+	}
+
+	private static String getPiggy(String piggy) {
+		return piggy;
 	}
 
 }
--- src/main/animals/Pig.java
+++ src/main/animals/Pig.java
@@ -4,15 +4,31 @@ package main.animals;
 public class Pig {
 
 	// CREATE the field "name" which is a String
+	private String name;
 	// CREATE the field "status" which is a String
-	
+	private String status;
+
 	public Pig(String name, String status) {
+		this.name = name;
+		this.status = status;
 		// CREATE the constructor
 	}
 	
 	
 	// Create getter and setter for name and status
-	
-	
+	public String getName(){
+		return name;
+	}
 
+	public String getStatus(){
+		return status;
+	}
+
+	public void setName(){
+		this.name = name;
+	}
+
+	public void setStatus(){
+		this.status = status;
+	}
 }
