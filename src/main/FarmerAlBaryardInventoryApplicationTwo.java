994468cc6cdc69eb38bd54920b1cc0967bb520ba
perjohnnym <juanjmore33@gmail.com>
2024-01-09 15:31:36 -0600

    changes - working code; this needs to be verified

--- src/main/FarmerAlBaryardInventoryApplicationTwo.java
+++ src/main/FarmerAlBaryardInventoryApplicationTwo.java
@@ -7,41 +7,46 @@ import java.util.Arrays;
 
 public class FarmerAlBaryardInventoryApplicationTwo {
 
+	// SORT the pigs by name...
+
+	// 1. run a for loop on the pig array
+	// 2. In the for loop create an array called pig names, get the names of the pigs using
+	// the getName getter from the Pig class, save it into the pigNames string array
+	// 3. Sort the pig names in Ascending order
+	// 4. Print the sorted names to the console
+
 	public static void main(String[] args) {
 		
 		FileService fileService = new FileService();
 		Pig[] pigs = fileService.getPigsFromFile();
+
 		String[] pigNames = new String[4];
 
-		pigNames[0]="Corkey";
-		pigNames[1]="Penny";
-		pigNames[2]="Bartholomew";
-		pigNames[3]="Rocky";
-
-
+		// In the for loop create an array called pig names, get the names of the pigs using
+		// the getName getter from the Pig class, save it into the pigNames string array
+		//we run thru the array of Pig objects and assign it to a String[] of pigNames so that we cqn later use the sort method
 		System.out.println("------This is PRE SORT --------");
-		System.out.println("Insert the code here that will write/display the recs as is:");
-
-
-		System.out.println("------This is POST SORT --ASC Order------");
-
-		Arrays.sort(pigNames);
-		for(String piggy : pigNames){
-			System.out.println(getPiggy(piggy));
+		int i=0;
+		for(Pig piggy : pigs){
+			System.out.println(pigNames[i]	= piggy.getName());
+			i++;
 		}
 
-		// SORT the pigs by name...
-		
-		// 1. run a for loop on the pig array
-		// 2. In the for loop create an array called pig names, get the names of the pigs using
-		// the getName getter from the Pig class, save it into the pigNames string array
-		// 3. Sort the pig names in Ascending order
-		// 4. Print the sorted names to the console
 
-	}
+		System.out.println("------This is where the POST SORT should appear --ASC Order------");
 
-	private static String getPiggy(String piggy) {
-		return piggy;
+		//when we define the @override CompareTo method, it's how we use the ".sort" method
+		//Here we need to sort an array of objects "Pig" and then in the CompareTo method, define how it's to be sorted
+		//However, the datatype 'String' already implements the class Comparable and has the default method "CompareTo"
+		//already defined (it is sorted in lexicographically)
+		//
+
+
+		Arrays.sort(pigNames);
+
+		for(String piggy : pigNames){
+				System.out.println(piggy);
+			}
 	}
 
 }
--- src/main/animals/Pig.java
+++ src/main/animals/Pig.java
@@ -1,17 +1,20 @@
 package main.animals;
 
 
-public class Pig {
+//Whenever we implement "Comparable", we must define its abstract method as well "compareTo"
+
+public class Pig implements Comparable <Pig>{
 
 	// CREATE the field "name" which is a String
 	private String name;
 	// CREATE the field "status" which is a String
 	private String status;
 
+	// CREATE the constructor
 	public Pig(String name, String status) {
 		this.name = name;
 		this.status = status;
-		// CREATE the constructor
+
 	}
 	
 	
@@ -23,12 +26,32 @@ public class Pig {
 	public String getStatus(){
 		return status;
 	}
+	
 
-	public void setName(){
-		this.name = name;
-	}
+	@Override
+	public int compareTo(Pig o) {
+		//put logic that compares it and sort it via asc - here we override the default functionality and we need to define how we want it to SORT in ascending order
+		System.out.println(	"we have reached the compareTo method: ");
 
-	public void setStatus(){
-		this.status = status;
+		// 1 (positive number)  - ascending order
+		// -1 (negative number) - descending order
+		// 0  (zero) - does not do any sorting
+
+		// need to call the 'compare' method
+		//compare this to that  (compare name to name)
+		//
+
+		//	return 	o.name.compareTo(this.name);
+
+
+/*		if (this.getName() > that.getName()) {
+           return -1;
+     	 } else if (this.name.equals(that.name)) {
+          return 0;
+     	 } else{
+           return  1;
+           }*/
+	return 0;
+
 	}
 }
